644ef799ba5e5455558c365875a890004a148374
Sreeram Chengaloor <sreeramchengaloor03@gmail.com>
2024-06-26 17:41:34 +0530

    :p

--- qrjungle/lib/pages/qrcardgrid.dart
+++ qrjungle/lib/pages/qrcardgrid.dart
@@ -2,7 +2,6 @@ import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:qrjungle/models/apiss.dart';
-import 'package:qrjungle/pages/bottomnavbar/profile.dart';
 import 'package:qrjungle/pages/moreqr/moreqr.dart';
 import 'package:qrjungle/pages/moreqr/widgets/modals.dart';
 
@@ -121,18 +120,14 @@ class _QrcardgridState extends State<Qrcardgrid> {
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
                     return GestureDetector(
-                      onTap: () {
-                        if (!loggedinmain) {
-                          LogInModalSheet(context);
-                        } else {
+                      onTap: () {                        
                           Navigator.push(
                             context,
                             MaterialPageRoute(
                                 builder: (context) => MoreQr(
                                     imageUrl: snapshot.data.toString(),
                                     item: item)),
-                          );
-                        }
+                          );                        
                       },
                       child: Container(
                         color: Colors.transparent,
