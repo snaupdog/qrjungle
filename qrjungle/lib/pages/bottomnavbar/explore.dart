8a20c5fb9ba501084f3df1611309a15246d5f146
snau <nihal.satish@gmail.com>
2024-06-28 13:18:56 +0530

    fakedata skeeletonizer workign

--- qrjungle/lib/pages/bottomnavbar/explore.dart
+++ qrjungle/lib/pages/bottomnavbar/explore.dart
@@ -1,8 +1,10 @@
 //import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:qrjungle/models/apiss.dart';
+import 'package:qrjungle/myshitcode/testpage.dart';
 import 'package:qrjungle/pages/MoreCategory.dart';
 import 'package:qrjungle/pages/qrcardgrid.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 //import 'package:qrjungle/pages/moreqr/moreqr.dart';
 
 class ExplorePage extends StatefulWidget {
@@ -14,6 +16,7 @@ class ExplorePage extends StatefulWidget {
 
 class _ExplorePageState extends State<ExplorePage> {
   List<String> categoryname = [];
+  List<String> fakedata = List.filled(12, "Hello");
   List<String> urls = [];
   String token = '';
   bool isLoading = true;
@@ -27,14 +30,14 @@ class _ExplorePageState extends State<ExplorePage> {
 
   categories() async {
     try {
-      var hi = await Apiss().getCategories();
+      var items = await Apiss().getCategories();
 
-      var items = hi;
       setState(() {
         // print(items);
         categoryname = items
             .map<String>((item) => item['category_name'] as String)
             .toList();
+        print(categoryname);
         isLoading = false;
       });
     } catch (e) {
@@ -49,6 +52,12 @@ class _ExplorePageState extends State<ExplorePage> {
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => const SkeletonizerDemoPage()));
+      }),
       body: SingleChildScrollView(
         physics: const AlwaysScrollableScrollPhysics(),
         child: Container(
@@ -60,65 +69,9 @@ class _ExplorePageState extends State<ExplorePage> {
               Text('Categories',
                   style: textTheme.bodyMedium
                       ?.copyWith(fontWeight: FontWeight.w600)),
-              Container(
-                child: GridView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            childAspectRatio: 1.08, crossAxisCount: 2),
-                    itemCount: categoryname.length,
-                    itemBuilder: (context, index) {
-                      return InkWell(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => CategoryPage(
-                                        catname: categoryname[index],
-                                        catimageurl: 'assets/qrsample.png',
-                                      )));
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.all(8),
-                          height: MediaQuery.sizeOf(context).height * 0.2,
-                          width: MediaQuery.sizeOf(context).width * 0.43,
-                          decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(13)),
-                            color: Color(0xff1B1B1B),
-                          ),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              SizedBox(
-                                  height:
-                                      MediaQuery.sizeOf(context).height * 0.13,
-                                  child: ClipRRect(
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(13)),
-                                    child: Image.asset('assets/qrsample.png',
-                                        width: double.infinity,
-                                        height: double.infinity,
-                                        fit: BoxFit.cover),
-                                  )),
-                              Padding(
-                                padding: const EdgeInsets.all(5.5),
-                                child: Text(
-                                    (categoryname[index]
-                                        .toString()
-                                        .replaceFirst(
-                                            categoryname[index][0],
-                                            categoryname[index][0]
-                                                .toUpperCase())),
-                                    style: textTheme.bodySmall?.copyWith(
-                                        fontWeight: FontWeight.w600)),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    }),
-              ),
+              isLoading
+                  ? newMethod(textTheme, fakedata)
+                  : newMethod(textTheme, categoryname),
               Text('All QR Codes',
                   style: textTheme.bodyMedium
                       ?.copyWith(fontWeight: FontWeight.w600)),
@@ -127,6 +80,65 @@ class _ExplorePageState extends State<ExplorePage> {
           ),
         ),
       ),
+    );
+  }
+
+  Skeletonizer newMethod(TextTheme textTheme, List<String> categoryname) {
+    return Skeletonizer(
+      enabled: isLoading,
+      enableSwitchAnimation: true,
+      child: GridView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              childAspectRatio: 1.08, crossAxisCount: 2),
+          itemCount: categoryname.length,
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CategoryPage(
+                              catname: categoryname[index],
+                              catimageurl: 'assets/qrsample.png',
+                            )));
+              },
+              child: Container(
+                margin: const EdgeInsets.all(8),
+                height: MediaQuery.sizeOf(context).height * 0.2,
+                width: MediaQuery.sizeOf(context).width * 0.43,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(13)),
+                  color: Color(0xff1B1B1B),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(
+                        height: MediaQuery.sizeOf(context).height * 0.13,
+                        child: ClipRRect(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(13)),
+                          child: Image.asset('assets/qrsample.png',
+                              width: double.infinity,
+                              height: double.infinity,
+                              fit: BoxFit.cover),
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.all(5.5),
+                      child: Text(
+                          (categoryname[index].toString().replaceFirst(
+                              categoryname[index][0],
+                              categoryname[index][0].toUpperCase())),
+                          style: textTheme.bodySmall
+                              ?.copyWith(fontWeight: FontWeight.w600)),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
     );
   }
 }
