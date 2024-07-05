7e05423f147f32745b32dc9c5e64849d7feddedd
snau <nihal.satish@gmail.com>
2024-07-05 11:43:50 +0530

    qrscanner fixed

--- qrjungle/macos/Flutter/GeneratedPluginRegistrant.swift
+++ qrjungle/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import amplify_auth_cognito
 import amplify_secure_storage
 import connectivity_plus
 import device_info_plus
+import mobile_scanner
 import package_info_plus
 import path_provider_foundation
 import share_plus
@@ -21,6 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AmplifySecureStoragePlugin.register(with: registry.registrar(forPlugin: "AmplifySecureStoragePlugin"))
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
